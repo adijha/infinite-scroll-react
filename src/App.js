ff3ad11a45acb5ee9fd02037e037aec61ded4723
Aditya Kumar Jha <mail@adijha.com>
2020-11-04 04:30:03 +0545

    ui skeleton ready

--- src/App.js
+++ src/App.js
@@ -3,20 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import React, { useEffect, useState, useRef } from "react";
 import { getImages } from "./dataAction";
 
-const divStyle = {
-	color: "blue",
-	height: "250px",
-	textAlign: "center",
-	padding: "5px 10px",
-	background: "#eee",
-	margin: "35px",
-	maxWidth: "250px",
-};
-
-const containerStyle = {
-	maxWidth: "1280px",
-	margin: "0 auto",
-};
 const InfiniteScroll = () => {
 	const dispatch = useDispatch();
 	const images = useSelector((state) => state.images);
@@ -51,27 +37,88 @@ const InfiniteScroll = () => {
 	};
 
 	return (
-		<div className="container" style={containerStyle}>
-			<div
-				style={{
-					display: "flex",
-					flexWrap: "wrap",
-				}}
-			>
-				{images &&
-					images.map((post, index) => {
-						return (
-							<div key={index} className="post" style={divStyle}>
-								<h2> {index} </h2>
-								<img src={post.download_url} style={{ height: "100px" }} />
-							</div>
-						);
-					})}
+		<div
+			style={{
+				display: "flex",
+				flexWrap: "wrap",
+				// alignItems: "center",
+				justifyContent: "center",
+			}}
+		>
+			{images &&
+				images.map((post, index) => {
+					return (
+						<div
+							key={index}
+							style={{
+								margin: "1rem",
+								// border: "solid 1px #eee",
+								position: "relative",
+								width: "25%",
+								height: "25%",
+				boxShadow:'unset 8px 8px 8px 8px #F1F1F1'
 
-				{/* <!-- Add Ref to Load More div --> */}
-				<div className="loading" ref={loader}>
-					<h2>Loading More</h2>
-				</div>
+							}}
+						>
+							<p
+								style={{
+									position: "absolute",
+									backgroundColor: "#7A2445",
+									color: "white",
+									fontSize: "12px",
+									padding: "5px",
+									right: "5px",
+									top: "-5px",
+								}}
+							>
+								MUSIC
+							</p>
+							<img
+								src={post.download_url}
+								style={{
+									width: "100%",
+									objectFit:'cover',
+									height:'13rem'
+								}}
+							/>
+							<div
+								style={{
+									padding: 0,
+									paddingLeft: "10px",
+									paddingRight: "10px",
+								}}
+							>
+								<p style={{ fontSize: "15px",fontWeight:'bold' }}>Barcadi NH7 Weekender 2020</p>
+								<p style={{ fontSize: "13px", color: "#666465" }}>
+									Devember 5 -6 | 7 PM
+								</p>
+								<p style={{ fontSize: "13px", color: "#666465" }}>
+									Live On Insider
+								</p>
+								<div
+									style={{
+										display: "flex",
+										alignItems: "flex-start",
+										justifyContent: "space-between",
+									}}
+								>
+									<p style={{ fontSize: "13px",fontWeight:'500' }}>$199 onwards</p>
+									<p style={{ fontSize: "20px", fontWeight: "bold" }}>
+										{index > 0 && index}{" "}
+										<i
+											class="fa fa-heart-o"
+											style={{ fontSize: "20px", fontWeight: "bold" }}
+										></i>
+									</p>
+								</div>
+							</div>
+						</div>
+					);
+				})}
+
+			{/* <!-- Add Ref to Load More div --> */}
+			<div className="loading" ref={loader}>
+				<h2>Loading More</h2>
 			</div>
 		</div>
 	);
