4323d747d70428d0490a55df8fd8dd088521784b
Aditya Kumar Jha <mail@adijha.com>
2020-11-04 08:33:14 +0545

    css ejected

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 import React, { useEffect, useState, useRef } from "react";
 import { getImages } from "./dataAction";
-import './App.css'
+import "./App.css";
 const InfiniteScroll = () => {
 	const dispatch = useDispatch();
 	const images = useSelector((state) => state.images);
@@ -37,108 +37,51 @@ const InfiniteScroll = () => {
 	};
 
 	return (
-		<div
-			style={{
-				display: "flex",
-				flexWrap: "wrap",
-				// alignItems: "center",
-				justifyContent: "center",
-			}}
-		>
-			{images &&
-				images.map((post, index) => {
-					return (
-						<div
-							key={index}
-							style={{
-								margin: "1rem",
-								// border: "solid 1px #eee",
-								position: "relative",
-								width: "25%",
-								height: "25%",
-								boxShadow: "0 0 15px #ededed",
-							}}
-						>
-							<p
-								style={{
-									position: "absolute",
-									backgroundColor: "#7A2445",
-									color: "white",
-									fontSize: "12px",
-									padding: "5px",
-									right: "5px",
-									top: "-5px",
-									zIndex:'1'
-								}}
-							>
-								MUSIC
-							</p>
-							<div className="container"
-							
-							>
-								<img
-									src={post.download_url}
-									style={{
-										width: "100%",
-										objectFit: "cover",
-										height: "13rem",
-									}}
-									alt="Avatar" class="image" 
-								/>
-								<div class="middle">
-									<div class="text">BUY</div>
+		<React.Fragment>
+			<div className="container">
+				{images &&
+					images.map((post, index) => {
+						return (
+							<div key={index} className="product">
+								<p className="tag">MUSIC</p>
+								<div className="img-container">
+									<img
+										src={post.download_url}
+										style={{
+											width: "100%",
+											objectFit: "cover",
+											height: "13rem",
+										}}
+										alt="Avatar"
+										class="image"
+									/>
+									<div class="middle">
+										<div class="text">BUY</div>
+									</div>
+								</div>
+								<div className="product-desc">
+									<p className="product-title">Barcadi NH7 Weekender 2020</p>
+									<p style={{ fontSize: "13px", color: "#666465" }}>
+										Devember 5 -6 | 7 PM
+									</p>
+									<p className="product-location">Live On Insider</p>
+									<div className="price-group">
+										<p className="price">$199 onwards</p>
+										<p className="likes">
+											{index > 0 && index}{" "}
+											<i class="fa fa-heart-o" className="likes"></i>
+										</p>
+									</div>
 								</div>
 							</div>
-							<div
-								style={{
-									padding: 0,
-									paddingLeft: "10px",
-									paddingRight: "10px",
-								}}
-							>
-								<p style={{ fontSize: "15px", fontWeight: "bold" }}>
-									Barcadi NH7 Weekender 2020
-								</p>
-								<p style={{ fontSize: "13px", color: "#666465" }}>
-									Devember 5 -6 | 7 PM
-								</p>
-								<p
-									style={{
-										fontSize: "13px",
-										color: "#666465",
-										marginBottom: "0.5rem",
-									}}
-								>
-									Live On Insider
-								</p>
-								<div
-									style={{
-										display: "flex",
-										alignItems: "flex-start",
-										justifyContent: "space-between",
-									}}
-								>
-									<p style={{ fontSize: "13px", fontWeight: "500" }}>
-										$199 onwards
-									</p>
-									<p style={{ fontSize: "20px", fontWeight: "bold" }}>
-										{index > 0 && index}{" "}
-										<i
-											class="fa fa-heart-o"
-											style={{ fontSize: "20px", fontWeight: "bold" }}
-										></i>
-									</p>
-								</div>
-							</div>
-						</div>
-					);
-				})}
-
-			{/* <!-- Add Ref to Load More div --> */}
-			<div className="loading" ref={loader}>
-				<h2>Loading More</h2>
+						);
+					})}
 			</div>
-		</div>
+			{/* <!-- Add Ref to Load More div --> */}
+			<div ref={loader}>
+				<h2 className="loading">Loading More</h2>
+			</div>
+		</React.Fragment>
 	);
 };
 
